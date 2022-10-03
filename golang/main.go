5acd9e9c02d94aa4cbf6431dceb55ef3d3490b8a
Louis <huynguyennbk@gmail.com>
2022-10-03 19:36:39 +0700

    golang: packages and functions

---
+++ golang/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"BackendGolang/test"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to the playgrond!")
+
+	test.Packages()
+	test.Functions()
+}
---
+++ golang/test/packages.go
@@ -0,0 +1,10 @@
+package test
+
+import (
+	"fmt"
+	"math"
+)
+
+func Packages() {
+	fmt.Printf("Run Test Packages With Math: %g \n", math.Sqrt(7))
+}
